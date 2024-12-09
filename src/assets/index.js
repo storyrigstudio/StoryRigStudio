f7bb930954be139c32b1e47e33c08f484def2960
Your Name <you@example.com>
2024-12-09 22:05:29 +0530

    added

---
+++ src/assets/index.js
@@ -0,0 +1,93 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import unreal from "./tech/unreal.png";
+import blender from "./tech/blender.png";
+import davinci from "./tech/davinci.png";
+import aftereffect from "./tech/aftereffect.png";
+import pp from "./tech/pp.png";
+import pt from "./tech/pt.png";
+import ps from "./tech/ps.png";
+import ai from "./tech/ai.png";
+import ue from "./tech/ue.png";
+import csda from "./company/csda.png";
+import fl from "./company/fl.png";
+import gfxlogo from "./gfxlogo.png";
+import model from "./model.png";
+import animation from "./animation.png";
+import vfx from "./vfx.png";
+import edit from "./edit.png";
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  unreal,
+  blender,
+  davinci,
+  aftereffect,
+  pp,
+  pt,
+  ps,
+  ai,
+  ue,
+  csda,
+  fl,
+  gfxlogo,
+  model,
+  animation,
+  vfx,
+  edit,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,228 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  unreal,
+  blender,
+  davinci,
+  aftereffect,
+  pp,
+  pt,
+  ps,
+  ai,
+  ue,
+  csda,
+  fl,
+  gfxlogo,
+  model,
+  animation,
+  vfx,
+  edit,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "3D Modeling",
+    icon: model,
+  },
+  {
+    title: "Animations",
+    icon: animation,
+  },
+  {
+    title: "Visual Effects",
+    icon: vfx,
+  },
+  {
+    title: "Video Editing",
+    icon: edit,
+  },
+];
+
+const technologies = [
+  {
+    name: "ue",
+    icon: ue,
+  },
+  {
+    name: "blender",
+    icon: blender,
+  },
+  {
+    name: "davinci",
+    icon: davinci,
+  },
+  {
+    name: "aftereffect",
+    icon: aftereffect,
+  },
+  {
+    name: "pp",
+    icon: pp,
+  },
+  {
+    name: "pt",
+    icon: pt,
+  },
+  {
+    name: "ps",
+    icon: ps,
+  },
+  {
+    name: "ai",
+    icon: ai,
+  },
+];
+const experiences = [
+  {
+    title: "Freelance",
+    icon: fl,
+    iconBg: "#383E56",
+    date: "Jan 2024 - Dec 2024",
+    points: [
+      "Creating immersive 3D animations and visually appealing edits for YouTube videos, enhancing storytelling and audience engagement.",
+      "Delivering end-to-end production, including animation design, video editing, sound integration, and post-production polishing, to meet high-quality standards.",
+      "Collaborating with clients to bring their creative visions to life, ensuring timely delivery and aligning projects with their branding and audience expectations.",
+    ],
+  },
+  {
+    title: "College Club Media Team",
+    icon: csda,
+    iconBg: "#E6DEDD",
+    date: "Aug 2023 - March 2025",
+    points: [
+      "Handling the creation of promo videos, event celebration videos, and engaging content to showcase club activities and achievements.",
+      "Developing 3D animations and motion graphic animations to visually enhance promotional materials and event highlights.",
+      "Collaborating with team members to conceptualize, plan, and execute media projects aligned with the club's goals and vision.",
+      "Managing editing and post-production tasks to deliver high-quality videos that effectively capture the essence of club events and initiatives.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Department Virtual Tour - Immersive 3D Experience",
+    description:
+      "Created a realistic 3D model of our department block using Blender and Unreal Engine, featuring accurate layouts, detailed textures, and interactive hotspots. Optimized for VR and web platforms to provide an engaging and immersive exploration experience.",
+    tags: [
+      {
+        name: "blender",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Unreal Engine",
+        color: "green-text-gradient",
+      },
+      
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
