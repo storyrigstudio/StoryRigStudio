de0ff3e4cb3cf31d5fb4d7b7d83ef1b91365d726
Your Name <you@example.com>
2024-12-22 19:05:06 +0530

    2nd commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,7 +47,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        "I'm a skilled 3D artist with expertise in 3D modeling, animation, visual effects, and video editing. I excel at creating immersive, high-quality visuals that bring ideas to life, leveraging advanced tools and techniques to deliver exceptional results. A quick learner and collaborator, I work closely with clients to produce efficient and engaging solutions tailored to their needs. Let's work together to transform your vision into reality!"
+        "I'm a skilled 3D artist with expertise in 3D modeling, 3D Environment Design, animation, visual effects, and video editing. I excel at creating immersive, high-quality visuals that bring ideas to life, leveraging advanced tools and techniques to deliver exceptional results. A quick learner and collaborator, I work closely with clients to produce efficient and engaging solutions tailored to their needs. Let's work together to transform your vision into reality!"
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
