51e6b3ce9a8511af496253e08519a0eae569fc18
Your Name <you@example.com>
2025-06-23 20:40:04 +0530

    My changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -120,7 +120,7 @@ const Contact = () => {
          <a href="https://wa.me/916384821366" target="_blank" rel="noopener noreferrer">
             <IoLogoWhatsapp size={30} className="text-white hover:text-green-600 transition" />
           </a>
-          <a href="https://www.instagram.com/galaxia_fx?igsh=NngydGJtcjZjNDRn" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.instagram.com/storyrig_studio/" target="_blank" rel="noopener noreferrer">
             <FaInstagram size={30} className="text-white hover:text-pink-500 transition" />
           </a>
           <a href="https://www.youtube.com/@StoryRigStudio" target="_blank" rel="noopener noreferrer">
